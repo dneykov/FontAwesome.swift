9e6067e91b2fdeb164c9efef1836fea6a202fdb5
Daniel Neykov <danielnkv@gmail.com>
2022-01-25 14:38:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
                 .process("Resources/Fonts"),
             ]
         ),
-        .target(
+        .executableTarget(
             name: "tools"
         )
     ]
